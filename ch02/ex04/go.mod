cde999e49cd1bd538806d1b46c180fab0b2816d2
progfay <shun1856shun@gmail.com>
2020-09-04 09:06:37 +0900

    feat: add ch02/ex04

---
+++ ch02/ex04/go.mod
@@ -0,0 +1,3 @@
+module github.com/progfay/go-training/ch02/ex04
+
+go 1.15
---
+++ ch02/ex04/main.go
@@ -0,0 +1,29 @@
+package main
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCountWithShift(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCountWithLoop(x uint64) int {
+	var c int = 0
+	for i := 0; i < 64; i++ {
+		c += int(x & 1)
+		x >>= 1
+	}
+	return int(c)
+}
