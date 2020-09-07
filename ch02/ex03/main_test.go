6e971c20c12c9d9514e8fd74493b113c31866ac2
progfay <shun1856shun@gmail.com>
2020-09-07 20:34:40 +0900

    fix(ch02/ex03): avoid optimization

--- ch02/ex03/main_test.go
+++ ch02/ex03/main_test.go
@@ -6,18 +6,19 @@ import (
 	popcount "github.com/progfay/go-training/ch02/ex03"
 )
 
-func Benchmark_PopCount(b *testing.B) {
-	var x uint64 = 0xDA18
+var x uint64 = 0xDA18
+var dst1, dst2 int
 
+func Benchmark_PopCount(b *testing.B) {
 	b.Run("with bit shift", func(b *testing.B) {
-		for i := 0; i < 1000000; i++ {
-			popcount.PopCountWithShift(x)
+		for i := 0; i < b.N; i++ {
+			dst1 += popcount.PopCountWithShift(x)
 		}
 	})
 
-	b.Run("strings.Join", func(b *testing.B) {
-		for i := 0; i < 1000000; i++ {
-			popcount.PopCountWithLoop(x)
+	b.Run("with loop", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			dst2 += popcount.PopCountWithLoop(x)
 		}
 	})
 }
