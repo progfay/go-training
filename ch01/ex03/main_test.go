af12d450589bc2083254d635c421a7bbbace91e5
progfay <shun1856shun@gmail.com>
2020-09-07 20:26:42 +0900

    fix(ch01/ex03): avoid optimization

--- ch01/ex03/main_test.go
+++ ch01/ex03/main_test.go
@@ -5,29 +5,28 @@ import (
 	"testing"
 )
 
-func Benchmark_LongStringConcat(b *testing.B) {
-	var slice = []string{
-		strings.Repeat("1", 1000000),
-		strings.Repeat("2", 1000000),
-		strings.Repeat("3", 1000000),
-		strings.Repeat("4", 1000000),
-		strings.Repeat("5", 1000000),
-	}
+var slice = []string{
+	strings.Repeat("1", 1000000),
+	strings.Repeat("2", 1000000),
+	strings.Repeat("3", 1000000),
+	strings.Repeat("4", 1000000),
+	strings.Repeat("5", 1000000),
+}
 
+var dst1, dst2 string
+
+func Benchmark_LongStringConcat(b *testing.B) {
 	b.Run("assignment operator", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < 100; i++ {
-			str := ""
+		for i := 0; i < b.N; i++ {
 			for _, s := range slice {
-				str += " " + s
+				dst1 += " " + s
 			}
 		}
 	})
 
 	b.Run("strings.Join", func(b *testing.B) {
-		b.ResetTimer()
-		for i := 0; i < 100; i++ {
-			_ = strings.Join(slice, " ")
+		for i := 0; i < b.N; i++ {
+			dst2 = strings.Join(slice, " ")
 		}
 	})
 }
