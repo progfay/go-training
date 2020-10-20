ce76a52140884531e617ff9956a7b8a00b0415cf
progfay <shun1856shun@gmail.com>
2020-10-20 22:16:56 +0900

    chore(ch06/ex03): impl

---
+++ ch06/ex03/intset/intersectwith_test.go
@@ -0,0 +1,64 @@
+package intset_test
+
+import (
+	"testing"
+
+	"github.com/progfay/go-training/ch06/ex03/intset"
+)
+
+func Test_IntSet_IntersectWith(t *testing.T) {
+	for _, testcase := range []struct {
+		title string
+		in    struct {
+			left  []int
+			right []int
+		}
+		want string
+	}{
+		{
+			title: "empty sets",
+			in: struct {
+				left  []int
+				right []int
+			}{
+				left:  []int{},
+				right: []int{},
+			},
+			want: "{}",
+		},
+		{
+			title: "no duplication",
+			in: struct {
+				left  []int
+				right []int
+			}{
+				left:  []int{1, 3, 5, 7},
+				right: []int{2, 4, 6, 8},
+			},
+			want: "{}",
+		},
+		{
+			title: "duplication",
+			in: struct {
+				left  []int
+				right []int
+			}{
+				left:  []int{1, 3, 5, 7},
+				right: []int{1, 3, 5, 7},
+			},
+			want: "{1 3 5 7}",
+		},
+	} {
+		t.Run(testcase.title, func(t *testing.T) {
+			left, right := &intset.IntSet{}, &intset.IntSet{}
+			left.AddAll(testcase.in.left...)
+			right.AddAll(testcase.in.right...)
+			left.IntersectWith(right)
+			got := left.String()
+
+			if got != testcase.want {
+				t.Errorf("want %s, got %s", testcase.want, got)
+			}
+		})
+	}
+}
---
+++ ch06/ex03/intset/intset.go
@@ -0,0 +1,111 @@
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func popCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Has(x int) bool {
+	if x < 0 {
+		return false
+	}
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	if x < 0 {
+		panic("add negative integer to IntSet")
+	}
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *IntSet) Len() int {
+	count := 0
+	for _, word := range s.words {
+		count += popCount(word)
+	}
+	return count
+}
+
+func (s *IntSet) Remove(x int) {
+	if x < 0 {
+		return
+	}
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] &= ^(1 << bit)
+}
+
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+func (s *IntSet) Copy() *IntSet {
+	ret := &IntSet{}
+	if s.words == nil {
+		return ret
+	}
+	ret.words = make([]uint64, len(s.words))
+	copy(ret.words, s.words)
+	return ret
+}
