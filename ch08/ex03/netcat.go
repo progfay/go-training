0f540b464b308c6b75db09746ff526f27fecaea9
progfay <shun1856shun@gmail.com>
2020-11-23 23:53:14 +0900

    feat(ch08/ex03): impl

---
+++ ch08/ex03/netcat.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	c, ok := conn.(*net.TCPConn)
+	if !ok {
+		log.Fatalf("conn is not TCPConn: %v", conn)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, c)
+		c.CloseRead()
+		log.Println("done")
+		done <- struct{}{}
+	}()
+	mustCopy(c, os.Stdin)
+	c.Close()
+	<-done
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
