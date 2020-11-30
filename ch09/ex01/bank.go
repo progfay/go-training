fbbeefa0b168d1d9e93454b783f668b4a28d91ec
progfay <shun1856shun@gmail.com>
2020-11-30 22:11:00 +0900

    feat(ch09/ex01): impl

---
+++ ch09/ex01/bank.go
@@ -0,0 +1,41 @@
+package bank
+
+var deposits = make(chan int)
+var withdraws = make(chan int)
+var available = make(chan bool)
+var balances = make(chan int)
+
+func Deposit(amount int) {
+	deposits <- amount
+}
+
+func Withdraw(amount int) bool {
+	if amount < 0 {
+		return false
+	}
+	withdraws <- amount
+	return <-available
+}
+
+func Balance() int {
+	return <-balances
+}
+
+func teller() {
+	var balance int
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+
+		case amount := <-withdraws:
+			available <- balance >= amount
+
+		case balances <- balance:
+		}
+	}
+}
+
+func init() {
+	go teller()
+}
