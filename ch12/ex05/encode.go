0baa2c22b414101aba4e3603b5e4af5f3316cb9f
progfay <shun1856shun@gmail.com>
2021-01-26 23:46:00 +0900

    feat(ch12/ex05): impl

---
+++ ch12/ex05/encode.go
@@ -0,0 +1,95 @@
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := encode(&buf, reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func encode(buf *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("null")
+
+	case reflect.Bool:
+		if v.Bool() {
+			fmt.Fprint(buf, "true")
+		} else {
+			fmt.Fprint(buf, "false")
+		}
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.Float32, reflect.Float64:
+		fmt.Fprintf(buf, "%f", v.Float())
+
+	case reflect.Complex64, reflect.Complex128:
+		c := v.Complex()
+		fmt.Fprintf(buf, `"#C(%f %f)"`, real(c), imag(c))
+
+	case reflect.String:
+		fmt.Fprintf(buf, "%q", v.String())
+
+	case reflect.Ptr:
+		return encode(buf, v.Elem())
+
+	case reflect.Array, reflect.Slice:
+		buf.WriteByte('[')
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteString(",")
+			}
+			if err := encode(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte(']')
+
+	case reflect.Struct:
+		buf.WriteByte('{')
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteByte(',')
+			}
+			fmt.Fprintf(buf, "%q:", v.Type().Field(i).Name)
+			if err := encode(buf, v.Field(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte('}')
+
+	case reflect.Map:
+		buf.WriteByte('{')
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				buf.WriteByte(',')
+			}
+			if key.Kind() != reflect.String {
+				return fmt.Errorf("key of map must be string")
+			}
+			fmt.Fprintf(buf, "%q:", key.String())
+			if err := encode(buf, v.MapIndex(key)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte('}')
+
+	default:
+		return fmt.Errorf("unsupported type: %s", v.Type())
+	}
+	return nil
+}
---
+++ ch12/ex05/encode_test.go
@@ -0,0 +1,53 @@
+package sexpr
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func Test_Marshal(t *testing.T) {
+	type Movie struct {
+		Title, Subtitle string
+		Year            int
+		Actor           map[string]string
+		Oscars          []string
+		Sequel          *string
+	}
+	in := Movie{
+		Title:    "Dr. Strangelove",
+		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
+		Year:     1964,
+		Actor: map[string]string{
+			"Dr. Strangelove":            "Peter Sellers",
+			"Grp. Capt. Lionel Mandrake": "Peter Sellers",
+			"Pres. Merkin Muffley":       "Peter Sellers",
+			"Gen. Buck Turgidson":        "George C. Scott",
+			"Brig. Gen. Jack D. Ripper":  "Sterling Hayden",
+			`Maj. T.J. "King" Kong`:      "Slim Pickens",
+		},
+		Oscars: []string{
+			"Best Actor (Nomin.)",
+			"Best Adapted Screenplay (Nomin.)",
+			"Best Director (Nomin.)",
+			"Best Picture (Nomin.)",
+		},
+	}
+
+	jsonBytes, err := Marshal(in)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	var out Movie
+	err = json.Unmarshal(jsonBytes, &out)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !reflect.DeepEqual(in, out) {
+		t.Errorf("want %#v, got %#v", in, out)
+	}
+}
