41285e9deffcc3d85293a2c455e4afeaa8eeafc7
progfay <shun1856shun@gmail.com>
2021-02-05 11:13:21 +0900

    refactor(ch12/ex01): use Example for test

--- ch12/ex01/display_test.go
+++ ch12/ex01/display_test.go
@@ -1,63 +1,30 @@
 package display
 
-import (
-	"bytes"
-	"io"
-	"os"
-	"testing"
-)
-
 type S struct {
 	a int
 	b int
 }
 
-func Test_Display_StructKeyMap(t *testing.T) {
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Fatal(err)
-	}
-	stdout := os.Stdout
-	os.Stdout = w
-
+func ExampleDisplay_StructKeyMap() {
 	m := make(map[S]int)
 	m[S{a: 0, b: 0}] = 0
 	m[S{a: 1, b: 1}] = 1
 
 	Display("m", m)
-
-	os.Stdout = stdout
-	w.Close()
-
-	var buf bytes.Buffer
-	io.Copy(&buf, r)
-
-	if buf.String() != "Display m (map[display.S]int):\nm[{\"a\": 0, \"b\": 0}] = 0\nm[{\"a\": 1, \"b\": 1}] = 1\n" {
-		t.Errorf("%q", buf.String())
-	}
+	// Unordered output:
+	// Display m (map[display.S]int):
+	// m[{"a": 0, "b": 0}] = 0
+	// m[{"a": 1, "b": 1}] = 1
 }
 
-func Test_Display_ArrayKeyMap(t *testing.T) {
-	r, w, err := os.Pipe()
-	if err != nil {
-		t.Fatal(err)
-	}
-	stdout := os.Stdout
-	os.Stdout = w
-
+func ExampleDisplay_ArrayKeyMap() {
 	m := make(map[[2]int]int)
 	m[[2]int{0, 0}] = 0
 	m[[2]int{1, 1}] = 1
 
 	Display("m", m)
-
-	os.Stdout = stdout
-	w.Close()
-
-	var buf bytes.Buffer
-	io.Copy(&buf, r)
-
-	if buf.String() != "Display m (map[[2]int]int):\nm[[0, 0]] = 0\nm[[1, 1]] = 1\n" {
-		t.Errorf("%q", buf.String())
-	}
+	// Unordered output:
+	// Display m (map[[2]int]int):
+	// m[[0, 0]] = 0
+	// m[[1, 1]] = 1
 }
