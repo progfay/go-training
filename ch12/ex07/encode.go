68672af7d7fe198b719c479eb25ef0718c1d32f1
progfay <shun1856shun@gmail.com>
2021-02-05 11:50:18 +0900

    fix(ch12/ex07): define Encoder

--- ch12/ex07/encode.go
+++ ch12/ex07/encode.go
@@ -3,8 +3,21 @@ package sexpr
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"reflect"
 )
+
+type Encoder struct {
+	w io.Writer
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+func (e *Encoder) Encode(v interface{}) error {
+	return encode(e.w, reflect.ValueOf(v))
+}
 
 func Marshal(v interface{}) ([]byte, error) {
 	var buf bytes.Buffer
@@ -14,10 +27,10 @@ func Marshal(v interface{}) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func encode(buf *bytes.Buffer, v reflect.Value) error {
+func encode(buf io.Writer, v reflect.Value) error {
 	switch v.Kind() {
 	case reflect.Invalid:
-		buf.WriteString("nil")
+		fmt.Fprint(buf, "nil")
 
 	case reflect.Bool:
 		if v.Bool() {
@@ -48,48 +61,48 @@ func encode(buf *bytes.Buffer, v reflect.Value) error {
 		return encode(buf, v.Elem())
 
 	case reflect.Array, reflect.Slice:
-		buf.WriteByte('(')
+		fmt.Fprint(buf, '(')
 		for i := 0; i < v.Len(); i++ {
 			if i > 0 {
-				buf.WriteByte(' ')
+				fmt.Fprint(buf, ' ')
 			}
 			if err := encode(buf, v.Index(i)); err != nil {
 				return err
 			}
 		}
-		buf.WriteByte(')')
+		fmt.Fprint(buf, ')')
 
 	case reflect.Struct:
-		buf.WriteByte('(')
+		fmt.Fprint(buf, '(')
 		for i := 0; i < v.NumField(); i++ {
 			if i > 0 {
-				buf.WriteByte(' ')
+				fmt.Fprint(buf, ' ')
 			}
 			fmt.Fprintf(buf, "(%s ", v.Type().Field(i).Name)
 			if err := encode(buf, v.Field(i)); err != nil {
 				return err
 			}
-			buf.WriteByte(')')
+			fmt.Fprint(buf, ')')
 		}
-		buf.WriteByte(')')
+		fmt.Fprint(buf, ')')
 
 	case reflect.Map:
-		buf.WriteByte('(')
+		fmt.Fprint(buf, '(')
 		for i, key := range v.MapKeys() {
 			if i > 0 {
-				buf.WriteByte(' ')
+				fmt.Fprint(buf, ' ')
 			}
-			buf.WriteByte('(')
+			fmt.Fprint(buf, '(')
 			if err := encode(buf, key); err != nil {
 				return err
 			}
-			buf.WriteByte(' ')
+			fmt.Fprint(buf, ' ')
 			if err := encode(buf, v.MapIndex(key)); err != nil {
 				return err
 			}
-			buf.WriteByte(')')
+			fmt.Fprint(buf, ')')
 		}
-		buf.WriteByte(')')
+		fmt.Fprint(buf, ')')
 
 	default:
 		return fmt.Errorf("unsupported type: %s", v.Type())
